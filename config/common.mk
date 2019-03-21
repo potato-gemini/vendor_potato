15c0204a65fb336b98e8970a4a4ecd9dcdb9df9a
IlDivinatore01 <simo.m2001@gmail.com>
2019-03-21 11:54:14 +0000

    vendor_potato: update lawnchair, add OnePlus Gallery as default
    Signed-off-by: IlDivinatore01 <simo.m2001@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -188,3 +188,12 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/potato/overlay/common
 
 # Packages
 include vendor/potato/config/packages.mk
+
+# OnePlus Gallery
+PRODUCT_PACKAGES += \
+    OnePlusGallery \
+
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+\
+    vendor/du/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
