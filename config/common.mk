61c7276b14a80c892f2980ae876de7be5773d01a
IlDivinatore01 <simo.m2001@gmail.com>
2019-04-06 10:11:03 +0000

    fix

--- config/common.mk
+++ config/common.mk
@@ -193,7 +193,4 @@ include vendor/potato/config/packages.mk
 PRODUCT_PACKAGES += \
     OnePlusGallery \
 
-PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
-\
-    vendor/du/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
